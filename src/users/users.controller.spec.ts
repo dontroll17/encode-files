b8316ce89b699e300083b82e0d36af26d592da85
dontroll17 <dontroll@yandex.ru>
2022-10-03 12:55:01 +0300

    feat: user controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,5 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { mockUserRepository } from './mocks/repository.mock';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -7,12 +11,41 @@ describe('UsersController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useFactory: mockUserRepository
+        }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('define method', () => {
+    it('should be defined', () => {
+      expect(controller).toBeDefined();
+    });
+  
+    it('should be getAll method', () => {
+      expect(controller.getAll).toBeDefined();
+    });
+  
+    it('should be getById method', () => {
+      expect(controller.getUserById).toBeDefined();
+    });
+
+    it('should be create method', () => {
+      expect(controller.createUser).toBeDefined();
+    })
+  
+    it('should be remove method', () => {
+      expect(controller.removeUser).toBeDefined();
+    });
+  
+    it('should be update method', () => {
+      expect(controller.updateUser).toBeDefined();
+    });
   });
 });
