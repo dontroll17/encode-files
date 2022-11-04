5eb6119e73950743c61dba61986e142f01001ca9
dontroll17 <dontroll@yandex.ru>
2022-11-04 20:23:51 +0300

    refactor: class properties

--- src/crypt/crypt.service.ts
+++ src/crypt/crypt.service.ts
@@ -5,18 +5,12 @@ import { randomBytes, createCipheriv, createDecipheriv, createHash } from 'crypt
 @Injectable()
 export class CryptService {
 
-    getValue() {
-        const algoritm = process.env.ALGORITM;
-        const key = createHash('sha256').update(process.env.KEY).digest('base64').substring(0,32);
-
-        return [algoritm, key];
-    }
+    algoritm = process.env.ALGORITM
+    key = createHash('sha256').update(process.env.KEY).digest('base64').substring(0,32)
 
     encrypt(buffer) {
-        const [algoritm, key] = this.getValue();
-
         const iv = randomBytes(8).toString('hex');
-        const cipher = createCipheriv(algoritm, key, iv);
+        const cipher = createCipheriv(this.algoritm, this.key, iv);
 
         let encrypted = cipher.update(buffer).toString('hex');
         encrypted += cipher.final('hex');
@@ -24,10 +18,8 @@ export class CryptService {
     }
 
     decrypt(string) {
-        const [algoritm, key] = this.getValue();
-
         const [encryptData, iv] = string.split(':');
-        const decipher = createDecipheriv(algoritm, key, iv);
+        const decipher = createDecipheriv(this.algoritm, this.key, iv);
 
         let decrypted = decipher.update(encryptData, 'hex', 'utf-8');
         decrypted += decipher.final('utf-8');
